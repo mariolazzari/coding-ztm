494ba31e720bee53d0e3c7c03fa6130837379d3e
Mario Lazzari <mario.lazzari@italcoeng.com>
2022-01-14 12:18:03 +0100

    Done lesson 30

---
+++ 03_O/nemo.js
@@ -0,0 +1,60 @@
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = Array(100).fill("nemo");
+
+function findNemo(array) {
+  //let t0 = performance.now();
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Nemo found at index " + i);
+    }
+  }
+
+  //let t1 = performance.now();
+
+  /*console.log(
+    "Call to find Nemo took " + (t1 - t0).toFixed(2) + " milliseconds"
+  );
+  */
+}
+
+// Number of operations
+// O(n) -> linear
+findNemo(nemo);
+findNemo(everyone);
+findNemo(large);
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+// O(n)
+function compressAll(boxes) {
+  boxes.forEach(box => {
+    console.log("Linear compressing...", box);
+  });
+}
+
+// O(1)
+function compressOne(box) {
+  console.log("Constant compressing...", box);
+}
+
+compressAll(boxes); // O(n)
+compressOne(boxes[0]); // O(1)
+
+function logFirst2(array) {
+  console.log(array[0]); // O(1)
+  console.log(array[1]); // O(1)
+}
+logFirst2(boxes); // O(1)
