e78e1a2582da685c6da0144ccc117207b6507868
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-16 21:09:23 +0100

    Done lesson 102

--- 08_list/LinkedList.js
+++ 08_list/LinkedList.js
@@ -37,6 +37,19 @@ class LinkedList {
     return values;
   }
 
+  // O(n)
+  traverseToIndex(index) {
+    let current = 0;
+    let currentNode = this.head;
+
+    while (current < index) {
+      currentNode = currentNode.next;
+      current++;
+    }
+
+    return currentNode;
+  }
+
   // O(1)
   append(value) {
     const newNode = new Node(value);
@@ -51,10 +64,37 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
+
+  insert(index, value) {
+    // check input
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    // create new node
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.next = follower;
+    this.length++;
+  }
+
+  // O(n)
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+  }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(200, 99);
+myLinkedList.insert(2, 22);
+console.log(myLinkedList.printList());
+myLinkedList.remove(2);
 console.log(myLinkedList.printList());
