baa2a864cfd13db1451d266fbcee786284ba7c7f
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-19 20:50:39 +0100

    Done lesson 142

--- 10_trees/trees.js
+++ 10_trees/trees.js
@@ -1,10 +1,11 @@
 const BinaryTree = require("./BinaryTree");
 
-const printTree = (_key, val) => {
-  if (val) {
-    return val;
-  }
-};
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
 
 let tree = new BinaryTree();
 tree.insert(9);
@@ -14,5 +15,6 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-console.log(JSON.stringify(tree, printTree, 1));
+console.log(traverse(tree.root));
 console.log("find:", tree.lookup(170));
+console.log("delete:", tree.remove(170));
