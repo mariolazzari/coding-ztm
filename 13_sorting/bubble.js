5b20598fd2ff61b315a74eb60ddbaea200a4f620
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-20 19:28:45 +0100

    Done lesson 173

---
+++ 13_sorting/bubble.js
@@ -0,0 +1,20 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+// O(n^2)
+function bubbleSort(array) {
+  const { length } = array;
+  let tmp;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
