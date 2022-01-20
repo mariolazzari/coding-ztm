0078e05078fb17dd3403e90debb5192b0a3a3310
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-20 21:24:18 +0100

    Done lesson 200

---
+++ 14_searching/linear.js
@@ -0,0 +1,14 @@
+// Linear search
+const beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
+
+beasts.indexOf("Godzilla");
+
+beasts.findIndex(function (item) {
+  return item === "Godzilla";
+});
+
+beasts.find(function (item) {
+  return item === "Godzilla";
+});
+
+beasts.includes("Godzilla");
